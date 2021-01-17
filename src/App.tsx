bc5d92b6a6237be4ff6261bdcc258020ae5ffaa8
Francis Lagares <francis.lagares@gmail.com>
2021-01-17 14:10:27 +0100

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Lavish Website</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App Component', () => {
+  test('renders without crashing', () => {
+    render(<App />);
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+  });
+});
